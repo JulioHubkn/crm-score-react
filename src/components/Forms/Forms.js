131faf13eb79cc3b729b6ee54f0f1e4abf1a96b8
Giovanni-Ostemberg <gigio.ostemberg@gmail.com>
2021-09-02 14:15:21 -0400

    Adiçao de elementos

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -6,7 +6,15 @@ export default function Forms({ changePage }) {
   const [checkEmails, setCheckEmails] = useState(false);
   const [checkApiKey, setCheckApiKey] = useState(false);
   const goNext = () => {
-    changePage("Load Report");
+    let checkTerms = document.getElementById("authorization").checked;
+
+    if (!checkTerms) {
+      window.alert(
+        "Por favor, continue apenas se concordar com nossos termos de uso"
+      );
+    } else {
+      changePage("Load Report");
+    }
   };
 
   const goBack = () => {
@@ -16,6 +24,18 @@ export default function Forms({ changePage }) {
     let email1 = document.getElementById("email1").value;
     let email2 = document.getElementById("email2").value;
     let hubspotAPIInput = document.getElementById("hubspotAPI").value;
+    let name = document.getElementById("name").value;
+    let companyName = document.getElementById("companyName").value;
+
+    let nameConfirmation = false;
+    let companyNameConfirmation = false;
+    if (name.length >= 5) {
+      nameConfirmation = true;
+    }
+    if (companyName.length >= 2) {
+      companyNameConfirmation = true;
+    }
+
     let emailPattern =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     let hubspotAPIPattern =
@@ -42,7 +62,9 @@ export default function Forms({ changePage }) {
     if (
       email1.match(emailPattern) &&
       email1 === email2 &&
-      hubspotAPIInput.match(hubspotAPIPattern)
+      hubspotAPIInput.match(hubspotAPIPattern) &&
+      nameConfirmation &&
+      companyNameConfirmation
     ) {
       setFormConfirmation(true);
     } else {
@@ -68,11 +90,23 @@ export default function Forms({ changePage }) {
           >
             <div className={css.inputContainer}>
               <label htmlFor="name">Nome Completo</label>
-              <input type="text" name="name" required />
+              <input
+                type="text"
+                id="name"
+                name="name"
+                required
+                onKeyUp={validateEmail}
+              />
             </div>
             <div className={css.inputContainer}>
               <label htmlFor="name">Nome da Empresa</label>
-              <input type="text" name="companyName" required />
+              <input
+                type="text"
+                id="companyName"
+                name="companyName"
+                required
+                onKeyUp={validateEmail}
+              />
             </div>
             <div className={css.inputContainer}>
               <label htmlFor="name">E-mail</label>
@@ -120,11 +154,33 @@ export default function Forms({ changePage }) {
                 onKeyUp={validateEmail}
               />
             </div>
+            <div class={css.check}>
+              <label>
+                <input
+                  type="checkbox"
+                  name="authorization"
+                  id="authorization"
+                  value="authorize_terms"
+                  required
+                  onClick={validateEmail}
+                />
+                <span style={{ fontSize: "1em" }}>
+                  Autorizo a leitura e utilização dos dados do meu HubSpot por
+                  parte da hubkn segundo{" "}
+                  <a
+                    href="https://sales.hubkn.com/privacy-policy"
+                    target="_blank"
+                  >
+                    Termos de uso
+                  </a>{" "}
+                </span>
+              </label>
+            </div>
             <div className={css.buttonContainerForms}>
               <div className={css.Navigation}>
                 <a className={css.back} onClick={goBack}>
                   &#8610;{" "}
-                  <span style={{ fontSize: "0.5em", marginBottom: "5px" }}>
+                  <span style={{ fontSize: "0.5em", marginTop: "-5px" }}>
                     Voltar
                   </span>
                 </a>
@@ -146,7 +202,9 @@ export default function Forms({ changePage }) {
                   disabled
                   onClick={goNext}
                 >
-                  <span style={{ fontSize: "18px" }}>Testar HubSpot</span>
+                  {/* <span style={{ fontSize: "18px" }}> */}
+                  Testar HubSpot
+                  {/* </span> */}
                 </button>
               )}
             </div>
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import css from "./welcome.module.css";
+import M from "materialize-css";
 
 export default function Welcome({ changePage }) {
   const goNext = () => {
     changePage("Forms");
     console.log(`Clicou para testar`);
   };
+
+  const openModal = () => {
+    const modal = document.getElementById("modal1");
+    var instance = M.Modal.getInstance(modal);
+    instance.open();
+  };
+
   return (
     <div className={css.welcomeContainer1}>
       <div className={css.imgContainer}>
@@ -16,8 +24,8 @@ export default function Welcome({ changePage }) {
           <h2>
             {/* Olá, eu sou <span style={{ color: "#fc9c04" }}>Jordan</span>, posso
             ajudá-lo a tirar o máximo de sua operação de vendas. */}
-            Usuário <span style={{ color: "#fc9c04" }}>HubSpot</span>, você
-            confia no preenchimento do seu
+            {/* Usuário <span style={{ color: "#fc9c04" }}>HubSpot</span>,  */}
+            Você confia no preenchimento do seu
             <span style={{ color: "#fc9c04" }}> CRM</span>?
           </h2>
         </div>
@@ -30,11 +38,31 @@ export default function Welcome({ changePage }) {
       </div>
       <div className={css.buttonContainerWelcome}>
         <div className={css.Navigation}>
-          {/* <a>&#9711;</a>
-          <a>&#9675;</a> */}
+          <a href="#modal1" onClick={openModal} style={{ fontSize: "1em" }}>
+            Não sou usuário hubSpot
+          </a>
+
+          <div id="modal1" class="modal modal-fixed-footer">
+            <div class="modal-content">
+              <div
+                class="pipedriveWebForms"
+                data-pd-webforms="https://webforms.pipedrive.com/f/1teqWfAPUU51AampDyVHoYxlsmdo83XT6bRMY1mWDsGkSCKEIZfC5dz14D290q6Gv"
+              ></div>
+            </div>
+            <div class="modal-footer">
+              <a
+                href="#!"
+                class="modal-close waves-effect waves-green btn-flat"
+              >
+                Agree
+              </a>
+            </div>
+          </div>
         </div>
         <button className={css.nextWelcome} onClick={goNext}>
-          <span style={{ fontSize: "20px" }}>Testar HubSpot &#10148; </span>
+          {/* <span style={{ fontSize: "20px" }}> */}
+          Testar HubSpot &#10148;
+          {/* </span> */}
         </button>
       </div>
     </div>
